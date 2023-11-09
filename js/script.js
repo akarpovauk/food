3853d4b0c886b34d288ef1d521dfa02eac4ebcd1
Alena <alena.karpov@gmail.com>
2023-11-09 15:01:16 +0000

    feat: add json-server to send forms data

--- js/script.js
+++ js/script.js
@@ -177,12 +177,10 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 		render() {
 			const element = document.createElement('div');
-			if (this.classes.length === 0) {
-				this.element = 'menu__item';
-				element.classList.add(this.element);
-			} else {
-				this.classes.forEach(className => element.classList.add(className));
+			if (!this.classes.includes('menu__item')) {
+				this.classes.push('menu__item');
 			}
+			this.classes.forEach(className => element.classList.add(className));
 			
 			element.innerHTML = `
 				<img src= ${this.src} alt= ${this.alt}>
@@ -198,35 +196,55 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	new MenuCard(
-		'img/tabs/vegy.jpg',
-		'vegy',
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		'.menu .container',
-		'menu__item'
-	).render();
+	const getResource = async (url) => {
+		const res = await fetch(url);
 
-	new MenuCard(
-		'img/tabs/elite.jpg',
-		'elite',
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		14,
-		'.menu .container',
-		'menu__item'
-	).render();
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+		return await res.json();
+	};
 
-	new MenuCard(
-		'img/tabs/post.jpg',
-		'post',
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		21,
-		'.menu .container',
+	getResource('http://localhost:3000/menu')
+		.then(data => {
+			data.forEach(({img, altimg, title, descr, price}) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
+		});
 
-	).render();
+	// axios
+	/* 	axios.get('http://localhost:3000/menu')
+		.then(data => {
+			data.data.forEach(({img, altimg, title, descr, price}) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
+		}); */
+
+	// cards without classes
+	
+	/* 	getResource('http://localhost:3000/menu')
+		.then(data => createCard(data));
+
+	function createCard(data){
+		data.forEach(({img, altimg, title, descr, price}) => {
+			const element = document.createElement('div');
+
+			element.classList.add('menu__item');
+
+			element.innerHTML = `
+			<img src= ${img} alt= ${altimg}>
+			<h3 class="menu__item-subtitle">${title}</h3>
+			<div class="menu__item-descr">${descr}</div>
+			<div class="menu__item-divider"></div>
+			<div class="menu__item-price">
+				<div class="menu__item-cost">Цена:</div>
+				<div class="menu__item-total"><span>${price}</span> грн/день</div>
+			</div>
+			`;
+
+			document.querySelector('.menu .container').append(element);
+		});
+	} */
 
 	// forms
 
@@ -239,10 +257,21 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	forms.forEach(item => {
-		postData(item);
+		bindPostData(item);
 	});
 
-	function postData(form) {
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: data
+		});
+		return await res.json();
+	};
+
+	function bindPostData(form) {
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
 			const statusMessage = document.createElement('img');
@@ -255,19 +284,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			const formData = new FormData(form);
 
-			const object = {};
-			formData.forEach(function(value, key) {
-				object[key] = value;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			fetch('server.php', {
-				method: 'POST',
-				headers: {
-					'Content-type': 'application/json'
-				},
-				body: JSON.stringify(object)
-			})
-				.then(data => data.text())
+			postData('http://localhost:3000/requests', json)
 				.then(data => {
 					console.log(data);
 					showThanksModal(message.success);
@@ -303,10 +322,12 @@ window.addEventListener('DOMContentLoaded', () => {
 			prevModalDialog.classList.add('show');
 			prevModalDialog.classList.remove('hide');
 			closeModal();
-
 		}, 2000);
 	}
 
+	/* 	fetch('http://localhost:3000/menu')
+		.then(data => data.json());
+		.then(res => console.log(res)); */
+
+
 });
-
-
